d4135e8da96999a3b2297790c4222ea104990c9d
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2023-10-06 11:11:42 -0300

    feat: finalizada classe departamento usando factory para inicializar o departamento com 10 funcionarios

--- trabgcs/src/Departamento.java
+++ trabgcs/src/Departamento.java
@@ -1,3 +1,24 @@
 public class Departamento {
-    
+    private TipoDepartamento Tipo;
+    private Funcionario[] Funcionarios;
+    public Departamento InicializarDepartamentoComDezFuncionarios(TipoDepartamento tipo) {
+        Funcionarios = new Funcionario[10];
+        for (int i = 0; i < 10; i++) {
+            Funcionarios[i] = new Funcionario();
+        }
+
+        this.Tipo = Tipo;
+        this.Funcionarios = Funcionarios;
+
+        return this;
+    }
+
+    enum TipoDepartamento {
+        Financeiro,
+        RH,
+        Engenharia,
+        Manutenção,
+        Desenvolvimento
+    }
 }
+
