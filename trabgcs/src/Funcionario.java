eee0ea2d69a4bdf4e2f4d64b750a6d95ba50746f
sillysecret <j.kriger002@edu.pucrs.br>
2023-10-09 15:50:58 -0300

    feat: adicionado feature de users

--- trabgcs/src/Funcionario.java
+++ trabgcs/src/Funcionario.java
@@ -1,3 +1,18 @@
-public class Funcionario extends User {
-    
+public class Funcionario {
+     private String nome; 
+     private Tipofunc Tipo; 
+     private int id; 
+     
+     public Funcionario(){} 
+     
+     public Funcionario(String nome,Tipofunc tipo, int id) { 
+        this.id = id;
+        this.Tipo = tipo; 
+        this.nome = nome;
+     }
+
+     enum Tipofunc {
+        Funcionario, 
+        Administrador
+     }
 }
