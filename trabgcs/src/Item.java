4144fa239ea2030742c16ecc2821f315de7ca9ab
guilhermedsa <gdsa.2k01@gmail.com>
2023-10-20 15:13:55 -0300

    feat: adicionando getters e setters para variaveis

--- trabgcs/src/Item.java
+++ trabgcs/src/Item.java
@@ -3,9 +3,25 @@ public class Item {
     private Double Valor;
 
     public Item CriarNovoItem(String descricao, Double valor) {
-       this.Descricao = descricao;
-       this.Valor = valor;
-       return this;
+        this.Descricao = descricao;
+        this.Valor = valor;
+        return this;
     }
-    
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public Double getValor() {
+        return Valor;
+    }
+
+    public void setValor(Double valor) {
+        Valor = valor;
+    }
+
 }
